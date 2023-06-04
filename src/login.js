f2a6560c77c3aaa81139bcd5aa9e982ce33c69a8
Vyctoria Karina <vyctoriak@gmail.com>
2023-06-04 19:21:34 +0200

    refatorando e usando o exemplo citando do professor Rommel Vieira

--- src/login.js
+++ src/login.js
@@ -1,80 +1,130 @@
-class Login {
-    constructor(form, fields) {
-        this.form = form;
-        this.fields = fields;
-        this.validateOnSubmit();
+//
+//
+// Disciplina: Trabalho Interdisciplinar - Aplicações Web
+// Professor: Rommel Vieira Carneiro (rommelcarneiro@gmail.com)
+//
+// Código LoginApp utilizado como exemplo para alunos de primeiro período 
+
+
+// Página inicial de Login
+const LOGIN_URL = "login.html";
+
+// Objeto para o banco de dados de usuários baseado em JSON
+var db_usuarios = {};
+
+// Objeto para o usuário corrente
+var usuarioCorrente = {};
+
+// função para gerar códigos randômicos a serem utilizados como código de usuário
+// Fonte: https://stackoverflow.com/questions/105034/how-to-create-guid-uuid
+function generateUUID() { // Public Domain/MIT
+    var d = new Date().getTime();//Timestamp
+    var d2 = (performance && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16;//random number between 0 and 16
+        if(d > 0){//Use timestamp until depleted
+            r = (d + r)%16 | 0;
+            d = Math.floor(d/16);
+        } else {//Use microseconds since page-load if supported
+            r = (d2 + r)%16 | 0;
+            d2 = Math.floor(d2/16);
+        }
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
+
+
+// Dados de usuários para serem utilizados como carga inicial
+const dadosIniciais = {
+    usuarios: [
+        { "id": generateUUID (), "senha": "123", "nome": "Juliana", "email": "juliana@gmail.com"},
+        { "id": generateUUID (), "senha": "123", "nome": "José", "email": "jose@gmail.com"},
+    ]
+};
+
+
+// Inicializa o usuarioCorrente e banco de dados de usuários da aplicação de Login
+function initLoginApp () {
+    // PARTE 1 - INICIALIZA USUARIOCORRENTE A PARTIR DE DADOS NO LOCAL STORAGE, CASO EXISTA
+    usuarioCorrenteJSON = sessionStorage.getItem('usuarioCorrente');
+    if (usuarioCorrenteJSON) {
+        usuarioCorrente = JSON.parse(usuarioCorrenteJSON);
     }
+    
+    // PARTE 2 - INICIALIZA BANCO DE DADOS DE USUÁRIOS
+    // Obtem a string JSON com os dados de usuários a partir do localStorage
+    var usuariosJSON = localStorage.getItem('db_usuarios');
 
-    validateOnSubmit() {
-        let self = this;
+    // Verifica se existem dados já armazenados no localStorage
+    if (!usuariosJSON) {  // Se NÃO há dados no localStorage
+        
+        // Informa sobre localStorage vazio e e que serão carregados os dados iniciais
+        alert('Dados de usuários não encontrados no localStorage. \n -----> Fazendo carga inicial.');
 
-        this.form.addEventListener('submit', (e) => {
-            e.preventDefault();
-            let error = 0;
-            self.fields.forEach((field) => {
-                const input = document.querySelector(`#${field}`);
-                console.log(input.value);
-                if(self.validateFields(input) == false) {
-                    error++;
-                }
-            })
-            if(error == 0) {
-                console.log('success');
-                this.form.submit();
-            }
-        })
+        // Copia os dados iniciais para o banco de dados 
+        db_usuarios = dadosIniciais;
+
+        // Salva os dados iniciais no local Storage convertendo-os para string antes
+        localStorage.setItem('db_usuarios', JSON.stringify (dadosIniciais));
     }
+    else  {  // Se há dados no localStorage
+        
+        // Converte a string JSON em objeto colocando no banco de dados baseado em JSON
+        db_usuarios = JSON.parse(usuariosJSON);    
+    }
+};
 
-    validateFields(field) {
-        if(field.value.trim() == "") {
-            this.setStatus(
-                field,
-                `${field.previousElementSibling.innerText} não pode estar vazio`,
-                // `O campo está vazio!`,
-                "error"
-            );
-            return false;
-        } else {
-            if(field.type == 'password') {
-                if (field.value.length < 8) {
-                    this.setStatus(
-                        field,
-                        `${field.previousElementSibling.innerText} precisa ter pelo menos 8 caractéres.`,
-                        "error"
-                    );
-                    return false;
-                } else {
-                    this.setStatus(field, null, "success");
-                    return true;
-                }
-            } else {
-                this.setStatus(field, null, "success");
-                return true;
-            }
+
+// Verifica se o login do usuário está ok e, se positivo, direciona para a página inicial
+function loginUser (email, senha) {
+    
+    // Verifica todos os itens do banco de dados de usuarios 
+    // para localizar o usuário informado no formulario de login
+    for (var i = 0; i < db_usuarios.usuarios.length; i++) {
+        var usuario = db_usuarios.usuarios[i];
+        
+        // Se encontrou login, carrega usuário corrente e salva no Session Storage
+        if (email == usuario.email && senha == usuario.senha) {
+            usuarioCorrente.id = usuario.id;
+            usuarioCorrente.email = usuario.email;
+            usuarioCorrente.nome = usuario.nome;
+            
+            // Salva os dados do usuário corrente no Session Storage, mas antes converte para string
+            sessionStorage.setItem ('usuarioCorrente', JSON.stringify (usuarioCorrente));
+
+            // Retorna true para usuário encontrado
+            return true;
         }
     }
 
-    setStatus(field, message, status) {
-        const errorMessage = field.parentElement.querySelector('.error-message');
+    // Se chegou até aqui é por que não encontrou o usuário e retorna falso
+    return false;
+}
 
-        if(status == 'success') {
-            if(errorMessage) {
-                errorMessage.innerText = ''
-            }
-            field.classList.remove('input-error');
-        }
+// Apaga os dados do usuário corrente no sessionStorage
+function logoutUser () {
+    usuarioCorrente = {};
+    sessionStorage.setItem('usuarioCorrente', JSON.stringify(usuarioCorrente));
+    window.location = LOGIN_URL;
+}
 
-        if(status == "error") {
-            errorMessage.innerText = message;
-            field.classList.add('input-error');
-        }
-    }
+function addUser (nome, email, senha) {
+    
+    // Cria um objeto de usuario para o novo usuario 
+    let newId = generateUUID ();
+    let usuario = { "id": newId, "nome": nome, "email": email, "senha": senha };
+    
+    // Inclui o novo usuario no banco de dados baseado em JSON
+    db_usuarios.usuarios.push (usuario);
+
+    // Salva o novo banco de dados com o novo usuário no localStorage
+    localStorage.setItem('db_usuarios', JSON.stringify (db_usuarios));
+}
+
+function setUserPass () {
 
 }
 
-const form = document.querySelector(".formulario_login");
 
-if(form) {
-    const fields = ['email', 'password'];
-    const validator = new Login(form, fields);
-}
+// Inicializa as estruturas utilizadas pelo LoginApp
+initLoginApp ();
